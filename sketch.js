38a82d37b4f69fbf27c33b60bb7818a3465451d7
Goyal-Raghav <98651943+Goyal-Raghav@users.noreply.github.com>
2022-01-29 19:57:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+
+  box=createSprite(200,200,20,20);
+}
+
+function draw() 
+{
+  background("red");
+  if(keyIsDown(DOWN_ARROW)){
+    box.y=box.y+1;
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    box.y=box.y-1;
+  }
+
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+1;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.x=box.x-1;
+  }
+
+
+  drawSprites();
+
+}
+
+
+
+
